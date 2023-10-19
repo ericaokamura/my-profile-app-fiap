b6a93cf1d87ebb1c485090d5c41f60f7b21631d0
ericaokamura <erica.okamura@yahoo.com.br>
2023-10-19 16:19:20 -0300

    Update Menu.tsx

--- app/shared/Menu.tsx
+++ app/shared/Menu.tsx
@@ -5,13 +5,9 @@ import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
-import { useRouter } from 'next/navigation';
+import Typography from '@mui/material/Typography'
 
-
-export default function () {
-
-    const { push } = useRouter();
+export default function Header() {
 
     return (
         <header className={styles.header}>
