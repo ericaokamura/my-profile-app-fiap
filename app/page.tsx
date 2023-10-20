b9b766bbe374ec2e21080654a0a1d63baef0e65c
ericaokamura <erica.okamura@yahoo.com.br>
2023-10-20 08:36:31 -0300

    corrigindo o width de divs

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <>
       <div style={{ display: "flex", justifyContent: "space-between", backgroundColor: "#F6C6CF", width: "auto", height: "fit-content", border: "8px solid #0F2CA0", marginBottom: "20px" }}>
-        <div style={{ width: "50%", height: "fit-content", marginLeft: "100px", display: "flex" }}>
+        <div style={{ width: "fit-content", height: "fit-content", marginLeft: "100px", display: "flex" }}>
           <Menu />
           <Image
             src={erica}
